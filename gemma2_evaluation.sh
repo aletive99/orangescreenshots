f516133e4b0f09c663038475c5200a54c1911894
Alessandro Tiveron <162694952+aletive99@users.noreply.github.com>
2024-07-24 15:08:07 +0200

    Update gemma2_evaluation.sh

--- gemma2_evaluation.sh
+++ gemma2_evaluation.sh
@@ -2,15 +2,12 @@
 
 #SBATCH --job-name=test_gemma2_27b      # Job name
 #SBATCH --partition=dev				    # Partition name, e.g., gpu
-#SBATCH --gres=gpu:4                    # Request 4 GPUs
+#SBATCH --gres=gpu:1                    # Request 4 GPUs
 #SBATCH --ntasks=1                      # Number of tasks
 #SBATCH --cpus-per-task=16              # Number of CPU cores per task
 #SBATCH --mem=64G                       # Memory per node
 #SBATCH --time=2:00:00                	# Time limit
 #SBATCH --output=results_%j.log         # Output log file (%j expands to jobID)
-
-# Start the Ollama server in the background
-ollama serve &
 
 # Run the Python script
 python3 evaluate_prompts.py
