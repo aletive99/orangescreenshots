22f76d623d4beaeaf69dbbb7829e7062da66aa81
Alessandro Tiveron <162694952+aletive99@users.noreply.github.com>
2024-07-24 15:36:10 +0200

    Add files via upload

--- gemma2_evaluation.sh
+++ gemma2_evaluation.sh
@@ -2,14 +2,13 @@
 
 #SBATCH --job-name=test_gemma2_27b      # Job name
 #SBATCH --partition=dev				    # Partition name, e.g., gpu
-#SBATCH --gres=gpu:6                    # Request 4 GPUs
-#SBATCH --ntasks=1                      # Number of tasks
-#SBATCH --cpus-per-task=8               # Number of CPU cores per task
+#SBATCH --gpus=A100_80GB:6              # Request 6 GPUs
+#SBATCH --cpus-per-task=16              # Number of CPU cores per task
 #SBATCH --mem=64G                       # Memory per node
 #SBATCH --time=2:00:00                	# Time limit
 #SBATCH --output=results_%j.log         # Output log file (%j expands to jobID)
 
-ollama serve > /dev/null &
+ollama serve > /dev/null 2>&1 &
 
 # Run the Python script
 python3 evaluate_prompts.py
