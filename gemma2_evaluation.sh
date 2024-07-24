a5a3e001472fc5a1ebebaf6271ca95974ff17440
Alessandro Tiveron <162694952+aletive99@users.noreply.github.com>
2024-07-24 13:02:35 +0200

    Update gemma2_evaluation.sh

--- gemma2_evaluation.sh
+++ gemma2_evaluation.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 #SBATCH --job-name=test_gemma2_27b      # Job name
-#SBATCH --partition=dev				    # Partition name, e.g., gpu
+#SBATCH --partition=dev				          # Partition name, e.g., gpu
 #SBATCH --gres=gpu:4                    # Request 4 GPUs
 #SBATCH --ntasks=1                      # Number of tasks
 #SBATCH --cpus-per-task=16              # Number of CPU cores per task
 #SBATCH --mem=64G                       # Memory per node
 #SBATCH --time=2:00:00                	# Time limit
 #SBATCH --output=results_%j.log         # Output log file (%j expands to jobID)
-
-# Load necessary modules (if any)
-module load cuda/11.3
 
 # Start the Ollama server in the background
 ollama serve &
