5f0acc694af5883bee39cc70f14e585d8ba4e0d9
Alessandro Tiveron <162694952+aletive99@users.noreply.github.com>
2024-07-24 15:29:28 +0200

    Add files via upload

--- evaluate_prompts.py
+++ evaluate_prompts.py
@@ -22,7 +22,8 @@ for type_of_desc in ['concise', 'detailed']:
             description_actual = widgets_info[name.split('/')[-1]]['concise']
         else:
             description_actual = widgets_info[name.split('/')[-1]]['detailed']
-        print('Evaluating the get_description function for the workflow: ' + name)
+        print1 = 'Evaluating the get_description function for the workflow: ' + name
+        print(print1)
         query = query_start
         query += '\n\nText 1:\n' + description_generated + '\n\nText 2:\n' + description_actual + '\n\nScore:'
         response = ollama.chat(
@@ -37,9 +38,11 @@ for type_of_desc in ['concise', 'detailed']:
             score = 10
         else:
             score = score[0]
-        print('The score for the get_description function is ' + str(score) + ' out of 10\n\n')
+        print2 = 'The score for the get_description function is ' + str(score) + ' out of 10\n\n'
+        print(print2)
         results += score
-    print('The get_description function got a score of ' + str(results) + ' %' + ' for description type' + type_of_desc + '\n--------------------\n\n')
+    print3 = 'The get_description function got a score of ' + str(results) + ' %' + ' for description type' + type_of_desc + '\n--------------------\n\n'
+    print(print3)
 
 
 
@@ -59,7 +62,8 @@ for name in filenames:
     if isinstance(workflows_info[name.split('/')[-1]]['goal'], list):
         for goal in workflows_info[name.split('/')[-1]]['goal']:
             possible_widgets = augment_widget_list(possible_widgets, present_widgets=workflow.get_widgets(), goal=goal)
-            print('Evaluating the new_widget_prompt function for the workflow: ' + name + ' for goal ' + goal)
+            print1 = 'Evaluating the new_widget_prompt function for the workflow: ' + name + ' for goal ' + goal
+            print(print1)
             target_widget = workflows_info[name.split('/')[-1]]['widget'][goal]
             prompt = get_new_widget_prompt(workflow, goal, True)
             response = ollama.chat(
@@ -81,16 +85,20 @@ for name in filenames:
                         n_correct += 1
                         break
                 if found == 0:
-                    print('The response does not contain the removed widget for the workflow: ' + name + ' for goal ' + goal)
+                    print2 = 'The response does not contain the removed widget for the workflow: ' + name + ' for goal ' + goal
+                    print(print2)
                     if not present:
-                        print('The widget is not in the possible widgets')
+                        print3 = 'The widget is not in the possible widgets'
+                        print(print3)
             else:
                 if target_widget in response:
                     n_correct += 1
                 else:
-                    print('The response does not contain the removed widget for the workflow: ' + name + ' for goal ' + goal)
+                    print2 = 'The response does not contain the removed widget for the workflow: ' + name + ' for goal ' + goal
+                    print(print2)
                     if Widget(target_widget) not in possible_widgets:
-                        print('The widget is not in the possible widgets')
+                        print3 = 'The widget is not in the possible widgets'
+                        print(print3)
             print()
     else:
         print('Evaluating the new_widget_prompt function for the workflow: ' + name)
@@ -117,17 +125,23 @@ for name in filenames:
                     n_correct += 1
                     break
             if found == 0:
-                print('The response does not contain the removed widget for the workflow: ' + name)
+                print2 = 'The response does not contain the removed widget for the workflow: ' + name + ' for goal ' + goal
+                print(print2)
                 if not present:
-                    print('The widget is not in the possible widgets')
+                    print3 = 'The widget is not in the possible widgets'
+                    print(print3)
             print('\n')
         else:
             if target_widget not in response:
-                print('The response does not contain the removed widget for the workflow: ' + name)
+                print2 = 'The response does not contain the removed widget for the workflow: ' + name + ' for goal ' + goal
+                print(print2)
                 if Widget(target_widget) not in possible_widgets:
-                    print('The widget is not in the possible widgets')
+                    print3 = 'The widget is not in the possible widgets'
+                    print(print3)
             else:
                 n_correct += 1
             print('\n')
-print('The new_widget_prompt function predicts ' + str(n_correct) + ' out of ' + str(count) + ' workflows correctly')
-print('The accuracy of the new_widget_prompt function is ' + str(n_correct/count*100)[:4] + '%')
+print4 = 'The new_widget_prompt function predicts ' + str(n_correct) + ' out of ' + str(count) + ' workflows correctly'
+print5 = 'The accuracy of the new_widget_prompt function is ' + str(n_correct/count*100)[:4] + '%'
+print(print4)
+print(print5)
--- gemma2_evaluation.sh
+++ gemma2_evaluation.sh
@@ -2,14 +2,14 @@
 
 #SBATCH --job-name=test_gemma2_27b      # Job name
 #SBATCH --partition=dev				    # Partition name, e.g., gpu
-#SBATCH --gres=gpu:6                    # Request 6 GPUs
+#SBATCH --gres=gpu:6                    # Request 4 GPUs
 #SBATCH --ntasks=1                      # Number of tasks
-#SBATCH --cpus-per-task=8              # Number of CPU cores per task
+#SBATCH --cpus-per-task=8               # Number of CPU cores per task
 #SBATCH --mem=64G                       # Memory per node
 #SBATCH --time=2:00:00                	# Time limit
 #SBATCH --output=results_%j.log         # Output log file (%j expands to jobID)
 
-ollama serve > /dev/null 2>&1 &
+ollama serve > /dev/null &
 
 # Run the Python script
 python3 evaluate_prompts.py
